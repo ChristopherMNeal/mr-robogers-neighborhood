11a1be8407f975f76b95e1e6ea13d50866bd2805
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-29 13:16:34 -0700

    Add name to results, add control if no name is given

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,10 @@ function makeNumberArray(input) {
 }
 
 function mrRobogerTranslator(numberInput) {
+  const name = $("input#name").val();
   const input = numberInput.toString();
   const triggerArray = ["3", "2", "1"];
-  const triggerTranslation = ["Won't you be my neighbor?", "Boop!", "Beep!"];
+  const triggerTranslation = ["Won't you be my neighbor, " + name + "?", "Boop!", "Beep!"];
   let output = "";
   for (let i = 0; i < triggerArray.length; i++) {
     for (let j = 0; j < input.length; j++) {
@@ -42,14 +43,18 @@ $(document).ready(function() {
   $("#name-form").submit(function(event) {
     event.preventDefault();
     name = $("input#name").val();
-    $("#name-form").slideToggle();
-    $("#number-form").delay(800).slideToggle();
+    if (!name) {
+      $("#no-name").show();
+    } else {
+      $("#name-form").slideToggle();
+      $("#number-form").delay(800).slideToggle();
+    }
   });
 
   $("#number-form").submit(function(event) {
     event.preventDefault();
     const number = $("input#number").val();
-    $("#results-p").html("Hi, " + name + "! " + "<br>" + mrRoboger(number));
+    $("#results-p").text(mrRoboger(number));
     $("#number-form").slideToggle();
     $("#results").delay(800).slideToggle();
     $("#reset").click(function() {
